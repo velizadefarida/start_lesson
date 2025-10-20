9f4648dd2a6846cfb418729bdc58ad8a14603d29
velizadefarida <velizadefarida@gmail.com>
2025-10-20 12:28:02 +0300

    i am cool

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
+
+
 int main()
-{}
+{
+  size_t rows = 0, cols = 0;
+  std::cin >> rows >> cols;
+  if (!std::cin ){
+    return 1;
+  }
+  std::cout << rows << " " << cols;
+  std::cout << "\n";
+}
