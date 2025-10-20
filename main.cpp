51a358fdcd40254658b4c254ced9668a99a3db57
velizadefarida <velizadefarida@gmail.com>
2025-10-20 12:50:25 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -9,11 +9,15 @@ int main()
 {
   size_t rows = 0, cols = 0;
   std::cin >> rows >> cols;
-  if (!std::cin ){
+  if (!std::cin ) {
     return 1;
   }
   int ** m = create (rows, cols);
   input (m, rows, cols);
+  if (!std::cin) {
+    remove(m, rows, cols);
+    return 1;
+  }
   output (m, rows, cols);
   remove (m, rows, cols);
   std::cout << "\n";
