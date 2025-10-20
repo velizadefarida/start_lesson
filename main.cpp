4bb0192e693b78c01a8af65aa4af6df6602466df
velizadefarida <velizadefarida@gmail.com>
2025-10-20 12:56:49 +0300

    3 commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@ void remove (int **m, size_t rows, size_t cols);
 void input (int ** m, size_t rows, size_t cols);
 void output (const int * const * m, size_t rows, size_t cols);
 
+void input (int ** m, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows && (std::cin); ++i) {
+    for (size_t j = 0; j < cols && (std::cin); ++j) {
+      std::cin >> m[i][j];
+    }
+  }
+}
+
 int main()
 {
   size_t rows = 0, cols = 0;
